3544196283723615c6d4ed72e38a06ff5163ebbc
Adam Tauber <asciimoo@gmail.com>
2023-04-21 16:58:13 +0200

    [fix] set correct domain for cookies

--- src/backend/endpoints/set_cookie.js
+++ src/backend/endpoints/set_cookie.js
@@ -1,5 +1,6 @@
 import { jar } from '../cookies.js';
 import { log } from '../logging.js';
+import url from 'node:url';
 
 
 // TODO authorize request
@@ -7,6 +8,11 @@ async function setCookie(req, resp) {
     if(req.query.cookie && req.query.url) {
         let cookie = req.query.cookie;
         try {
+            let host = req.headers.host.split(':')[0];
+            if(cookie.search(`domain=${host};`) > -1) {
+                let u = url.parse(req.query.url);
+                cookie = cookie.replace(`domain=${host};`, `domain=${u.host};`);
+            }
             jar.setCookieSync(cookie, req.query.url);
         } catch(err) {
             log.error("Failed to set cookie", err, cookie, req.query.url);
